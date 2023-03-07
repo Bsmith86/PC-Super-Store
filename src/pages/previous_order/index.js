4f5ca4067194d5bdf08374c991717c934ef944d9
bsmith86dev <bsmith86.dev@gmail.com>
2023-03-07 09:07:39 -0600

    Previous Order Styling

--- src/pages/previous_order/index.js
+++ src/pages/previous_order/index.js
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react'
 import { AppContext } from '../../context/app_context';
 import Cart from '../../components/cart';
-// import './index.css'
+import './index.css'
 
 
 const Previous = () => {
@@ -15,11 +15,17 @@ const Previous = () => {
 
     let ordersJSX = orders.map((el) => {
         return (
-            <div key={el._id}>
+            <div key={el._id} className='pOrders'>
                 Order Date:  {el.createdAt}
-                Qty:  {el.totalQty}
+                {/* Qty:  {el.totalQty}
                 Order Total:  {el.orderTotal}
-               
+                 */}
+                <div className='qty'>
+                    Items in order: {el.totalQty}
+                </div>
+                <div className='price'>
+                    Total Price: {el.orderTotal}
+                </div>
             </div>
             
         )
